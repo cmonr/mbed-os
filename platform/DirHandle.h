da5b09e466d092490987b795791d4003393d4a4e
Amanda Butler <Amanda.Butler@arm.com>
2018-10-17 17:41:27 -0500

    Fix typo in DirHandle.md
    Add "s" for agreement.

--- platform/DirHandle.h
+++ platform/DirHandle.h
@@ -120,7 +120,7 @@ public:
         return close();
     };
 
-    /** Return the directory entry at the current position, and
+    /** Returns the directory entry at the current position, and
      *  advances the position to the next entry.
      *
      * @returns
