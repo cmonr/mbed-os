674fae48461ac4a87460e6182f15f94ef531968d
Cruz Monrreal II <Cruz.Monrreal@arm.com>
2019-02-20 21:22:21 -0600

    Migrated source_gcc function to its own file. Windows doesn't have /dev/stdin...

---
+++ tools/test/travis-ci/functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -euf
+
+source_gcc()
+{
+  local _gcc_path="${1}"
+  local _filename="${2}"
+
+  if [ ! -d "${_gcc_path}" ]; then
+
+    printf "Downloading ${_filename}\n"
+    curl --location "${deps_url}/${_filename}" --output "${_filename}"
+
+    echo "Extracting ${_filename}"
+    mkdir -p "${HOME}/.cache/compilers"
+    [[ "${TRAVIS_OS_NAME}" == "linux" ]] \
+      && tar -xf "${_filename}" -C "${HOME}/.cache/compilers" \
+      || unzip "${_filename}" -d "${_gcc_path}"
+    rm "${_filename}"
+
+  fi
+}
+
